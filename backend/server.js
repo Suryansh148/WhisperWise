2bd97169e4a89b7a002d90c639dffc050dff4667
Suryansh148 <suryanshsinghchauhan148@gmail.com>
2023-07-03 15:55:20 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -23,11 +23,12 @@ app.use('/api/message',messageRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-// deploy
+// deploy-----------------------------------------------------------
 app.use(express.static(path.join(__dirname,"./frontend/build")));
 app.get("*", function(req,res){
   res.sendFile(path.join(__dirname,"./frontend/build/index.html"));
 })
+//------------------------------------------------------------------
 
 const PORT = process.env.PORT || 5000
 const server = app.listen(PORT);
